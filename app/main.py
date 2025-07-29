fd7192f9f5bb621709a5c9e2b270635b5d4c264f
malkeu <kangmark.2008@gmail.com>
2025-07-29 23:07:14 +0900

    Refactor Project Structure adaptive to FastAPI

---
+++ app/main.py
@@ -0,0 +1,17 @@
+from typing import Union
+
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.mount("/", StaticFiles(directory="app/templates", html=True), name="static")
+
+# @app.get("/")
+# def read_root():
+    
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
