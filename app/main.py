9537a72c0b0a8cb3234536d3b8ec8ee058d7bfe7
malkeu <kangmark.2008@gmail.com>
2025-08-05 22:00:30 +0900

    Make Basic Backend Structure

--- app/main.py
+++ app/main.py
@@ -2,16 +2,46 @@ from typing import Union
 
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+
+import pydantic
+from random import randint
+
 
 app = FastAPI()
 
-app.mount("/", StaticFiles(directory="app/templates", html=True), name="static")
 
-# @app.get("/")
-# def read_root():
+test_database = {}
+
+class DataRequest(pydantic.BaseModel):
+    session: int
+    data: str
+
+@app.get('/api/make-session')
+async def make_session():
+    session = randint(0, 9999)
+    while session in test_database.keys():
+        session = randint(0, 9999)
+    test_database[session] = 'nothing'
+    return {'session': session}
+
+@app.post('/api/write-data')
+async def some_request(request: DataRequest):
+    test_database[request.session] = request.data
+    return {"status": "success"}
     
 
+@app.get('/api/view-data')
+async def view_data(session: int):
+    return {"data": test_database.get(session, "No data found")}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+
+
+### 메인 메뉴 화면 ###
+app.mount("/mola", StaticFiles(directory="app/templates/mola", html=True))
+app.mount("/", StaticFiles(directory="app/templates", html=True))
+
+# 일반 파이썬으로 실행했을 때
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run('main:app', port=1234)
