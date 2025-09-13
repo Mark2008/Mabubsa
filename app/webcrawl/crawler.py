bfd8dec4f56a314641d368f59ad1f7f145c321a0
malkeu <kangmark.2008@gmail.com>
2025-09-13 17:52:07 +0900

    Cleanup Web Crawling

--- app/webcrawl/crawler.py
+++ app/webcrawl/crawler.py
@@ -1,5 +1,36 @@
-from bs4 import BeautifulSoup
+from typing import Dict
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
-def print_hello():
-    print('Hello!!')
+def cn_serachquery(query: str) -> Dict[str, str]:
+    driver = webdriver.Firefox()
+    driver.get(f"https://casenote.kr/search/?q={query}")
+
+    elems = driver.find_elements(By.CLASS_NAME, 'title')
+
+    to_find = []
+
+    for elem in elems:
+        try:
+            e2 = elem.find_element(By.TAG_NAME, 'a')
+            child_link = e2.get_attribute('href')
+            to_find.append(child_link)
+            
+        except Exception as e:
+            pass
+
+    saved = {}
+
+    for url in to_find:
+        driver.get(url)
+
+        title = driver.find_element(By.CLASS_NAME, 'cn-case-title')
+        content = driver.find_element(By.CLASS_NAME, 'cn-case-body')
+
+        saved[title] = content
+
+    driver.close()
+
+    return saved
