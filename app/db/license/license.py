17a1fa301c7ac2c9b9cfe055885a06b45b07c53b
malkeu <kangmark.2008@gmail.com>
2025-09-14 12:08:17 +0900

    Fix Function Call

--- app/db/license/license.py
+++ app/db/license/license.py
@@ -1,10 +1,18 @@
-from typing import Callable
+from typing import Dict, Callable
 
 import requests
 from bs4 import BeautifulSoup
 
 from db.cached_file import cached_file
 
+
+LICENSE_MAP: Dict[str, Callable] = {}
+
+def add_map(key: str):
+    def wrapper(func: Callable):
+        LICENSE_MAP[key] = func
+        return func
+    return wrapper
 
 # Creative Commons ----------------------------
 def __soup_cc(url: str, cl: str) -> str:
@@ -20,19 +28,22 @@ def __soup_cc(url: str, cl: str) -> str:
     return text
 
 def __cc_factory_4_0(slug: str) -> Callable:
-    @cached_file(f'app/db/license/cc-{slug}-4.0.txt')
+    @add_map(f'CC {slug.upper()} 4.0 legalcode')
+    @cached_file(f'app/db/license/cache/cc-{slug}-4.0.txt')
     def get_sth():
-        url = f'https://creativecommons.org/licenses/cache/{slug}/4.0/legalcode.ko'
+        url = f'https://creativecommons.org/licenses/{slug}/4.0/legalcode.ko'
         return __soup_cc(url, "#legal-code-body")
     return get_sth
         
 def __cc_deed_factory_4_0(slug: str) -> Callable:
-    @cached_file(f'app/db/license/cc-{slug}-4.0.deed.txt')
+    @add_map(f'CC {slug.upper()} 4.0 deed')
+    @cached_file(f'app/db/license/cache/cc-{slug}-4.0.deed.txt')
     def get_sth():
-        url = f'https://creativecommons.org/licenses/cache/{slug}/4.0/deed.ko'
+        url = f'https://creativecommons.org/licenses/{slug}/4.0/deed.ko'
         return __soup_cc(url, "#deed-body")
     return get_sth
 
+@add_map(f'About CC')
 @cached_file('app/db/license/cache/cc.about.txt')
 def get_cc_about():
     url = 'https://creativecommons.org/licenses/by/4.0/legalcode.ko'
@@ -53,3 +64,14 @@ get_cc_by_nc_nd_deed_4_0 = __cc_deed_factory_4_0('by-nc-nd')
 get_cc_by_nc_sa_deed_4_0 = __cc_deed_factory_4_0('by-nc-sa')
 get_cc_by_nd_deed_4_0 = __cc_deed_factory_4_0('by-nd')
 get_cc_by_sa_deed_4_0 = __cc_deed_factory_4_0('by-sa')
+
+
+# 펑션콜 전용 함수
+def find_license(license: str):
+    if license in LICENSE_MAP.keys():
+        return LICENSE_MAP[license]()
+    else:
+        return {'error': 'cannot find requested function'}
+
+def get_available_license():
+    return list(LICENSE_MAP.keys())
